973b3d5588778a98e1401bb70bed309846371c4a
Colton Rogers <crrogers12@gmail.com>
2022-05-21 10:25:50 -0700

    Made button reactive with the computed property

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ const app = Vue.createApp({
       visibleList: true,
     };
   },
+  computed: {
+    buttonCaption() {
+      return this.visibleList ? "Hide List" : "Show List";
+    },
+  },
   methods: {
     addTask() {
       this.tasks.push(this.enteredValue);
     },
     toggle() {
-     this.visibleList = !this.visibleList
+      this.visibleList = !this.visibleList;
     },
   },
 });
